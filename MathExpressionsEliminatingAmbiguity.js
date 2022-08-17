3cd8f02334a426dcc43c930f08cb03f1dcf30ad4
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-17 00:01:30 -0400

    1-6 of math expressions eliminating ambiguity

---
+++ MathExpressionsEliminatingAmbiguity.js
@@ -0,0 +1,35 @@
+// calculatedNum = 2 + (2 * 6);
+// What is the value of calculatedNum?
+
+calculatedNum = 14;
+
+// calculatedNum = (2 + 2) * 6;
+// What is the value of calculatedNum?
+
+calculatedNum = 24;
+
+// calculatedNum = (2 + 2) * (4 + 2);
+// What is the value of calculatedNum?
+
+calculatedNum = 24;
+
+// calculatedNum = ((2 + 2) * 4) + 2;
+// What is the value of calculatedNum?
+
+calculatedNum = 18;
+
+// Write a statement that assigns to num the result of 10 + 2 * 4, clarified with parentheses, producing 18. The variable hasn't been declared beforehand.
+
+var num = (2*4) + 10;
+
+// Write a statement that assigns to cost the result of 2 + 2 * 4 + 10, clarified with parentheses, producing 56. The variable hasn't been declared beforehand.
+
+var cost = (2+2) * (4+10);
+
+// Write a statement that assigns to units the result of 2 + 2 * 4 + 10, clarified with parentheses, producing 20. The variable hasn't been declared beforehand.
+
+var units = 2 + (2 * 4) + 10;
+
+// Write a statement that assigns to pressure the result of 4 / 2 * 4, clarified with parentheses, producing .5. The variable hasn't been declared beforehand.
+
+var pressure = 4 / (2 * 4);
