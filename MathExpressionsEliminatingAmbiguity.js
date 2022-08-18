bce0202496b11f5c4ea639793564b0d7a5631e2e
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-18 00:13:38 -0400

    final commit for math expressions elimating amb.

--- MathExpressionsEliminatingAmbiguity.js
+++ MathExpressionsEliminatingAmbiguity.js
@@ -33,3 +33,43 @@ var units = 2 + (2 * 4) + 10;
 // Write a statement that assigns to pressure the result of 4 / 2 * 4, clarified with parentheses, producing .5. The variable hasn't been declared beforehand.
 
 var pressure = 4 / (2 * 4);
+
+// Write a statement that assigns to pressure the result of 4 / 2 * 4, clarified with parentheses, producing 8. The variable hasn't been declared beforehand.
+
+var pressure = (4/2) * 2;
+
+// Write a statement that assigns to a variable the result of adding two variables together and then afterward multiplying the result of the addition by another variable. The first variable has been declared beforehand.
+
+total = (cost * time) * distance;
+
+// Write a statement that assigns to a variable the result of 4 / 2 * 4 - 1, clarified with parentheses, producing -.5. The variable hasn't been declared beforehand.
+
+var x = 4 / (2 * 4) - 1;
+
+// Time goal: 40 seconds. (The timer starts when you click in the answer field.) Write a statement that assigns to a variable the result of the following calculation: 2 numbers added together, with the result multiplied by a third number. The variable hasn't been declared beforehand.
+
+var number = (2 + 2) * 2; 
+
+// Time goal: 45 seconds. Write a statement that assigns to units the result of 2 + 2 * 4 + 10, clarified with parentheses, producing 20. The variable hasn't been declared beforehand.
+
+var units = 2 + (2 * 4) + 10;
+
+// Time goal: 40 seconds. Write a statement that assigns to pressure the result of 4 / 2 * 4, clarified with parentheses, producing .5. The variable hasn't been declared beforehand.
+
+var pressure = 4 / (2 * 4);
+
+// Time goal: 40 seconds. Write a statement that assigns to pressure the result of 4 / 2 * 4, clarified with parentheses, producing 8. The variable hasn't been declared beforehand.
+
+var pressure = (4 / 2) * 4;
+
+// Time goal: 50 seconds. Write a statement that assigns to a variable the result of adding two variables together and then afterward multiplying the result of the addition by another variable. The first variable has been declared beforehand.
+
+cost = (time + travel) * distance;
+
+// Time goal: 60 seconds. Write a statement that assigns to a variable the result of 4 / 2 * 4 - 1, clarified with parentheses, producing -.5. The variable hasn't been declared beforehand.
+
+var x = (4 / (2 * 4)) - 1;
+
+// Write a statement that assigns to units the result of 2 + 2 * 4 + 10, clarified with parentheses, producing 20. The variable hasn't been declared beforehand.
+
+var units = 2 + (2 * 4) + 10 ;
